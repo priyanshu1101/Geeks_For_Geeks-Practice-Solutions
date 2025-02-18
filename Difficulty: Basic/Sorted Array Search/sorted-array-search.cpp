127ca4b5fbd2e240e700050d0f3cebcbb315508d
priyanshu1101 <77241116+priyanshu1101@users.noreply.github.com>
2025-02-19 01:53:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sorted Array Search/sorted-array-search.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to find element in sorted array
+    // arr: input array
+    // k: element to be searched
+    bool searchInSorted(vector<int>& arr, int k) {
+
+        int start =0,end=arr.size()-1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(arr[mid]==k) return true;
+            else if(arr[mid]>k) end=mid-1;
+            else start=mid+1;
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(void) {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        int k;
+        cin >> k;
+        cin.ignore();
+        Solution ob;
+        cout << (ob.searchInSorted(arr, k) ? "true" : "false") << endl;
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
