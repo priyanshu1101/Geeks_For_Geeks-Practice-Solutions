bcb730e29abe67cbfd09c03a1a2d9cd0fe0bc450
priyanshu1101 <77241116+priyanshu1101@users.noreply.github.com>
2025-02-19 00:25:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.cpp
+++ Difficulty: Medium/Merge Sort/merge-sort.cpp
@@ -6,32 +6,34 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    void mergeSortedArray(vector<int>& arr,int start,int mid,int end){
-        // array1 start-mid;
-        // array2 mid+1-end;
-        vector<int> subans;
-        int aStart=start;
-        int aEnd=mid;
-        int bStart=mid+1;
-        int bEnd=end;
-        while(aStart<=aEnd && bStart<=bEnd){
-            if(arr[aStart]<arr[bStart]){
-                subans.push_back(arr[aStart++]);
+    void mergeTwoSorted(vector<int>& arr,int l,int mid,int r){
+        int s1=l,e1=mid,s2=mid+1,e2=r;
+        vector<int> subAns;
+        int tempStart = s1;
+        while(s1<=e1 && s2<=e2){
+            if(arr[s1]<arr[s2]){
+                subAns.push_back(arr[s1++]);
             }
-            else subans.push_back(arr[bStart++]);
+            else{
+                subAns.push_back(arr[s2++]);
+            }
         }
-        while(aStart<=aEnd) subans.push_back(arr[aStart++]);
-        while(bStart<=bEnd) subans.push_back(arr[bStart++]);
-        for(int i=0;i<subans.size();i++){
-            arr[start+i]=subans[i];
+        while(s1<=e1){
+            subAns.push_back(arr[s1++]);
+        }
+        while(s2<=e2){
+            subAns.push_back(arr[s2++]);
+        }
+        for(int i=0;i<subAns.size();i++){
+            arr[tempStart++] = subAns[i];
         }
     }
     void mergeSort(vector<int>& arr, int l, int r) {
-       if(l>=r) return;
-       int mid=(l+r)/2;
-       mergeSort(arr,l,mid);
-       mergeSort(arr,mid+1,r);
-       mergeSortedArray(arr,l,mid,r);
+        if(l>=r) return;
+        int mid = (l+r)/2;
+        mergeSort(arr,l,mid);
+        mergeSort(arr,mid+1,r);
+        mergeTwoSorted(arr,l,mid,r);
     }
 };
 
